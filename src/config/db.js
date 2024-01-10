83f57311f5ac4a4ecc063ea912e313ce4eace43c
diego7547 <diegoenriqueaccarapiquino@gmail.com>
2024-01-10 00:31:57 -0500

    a

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,8 @@ const conexion = createPool(
         port:process.env.PORTDB,
         user:process.env.USERDB,
         password:process.env.PASSWORD,
-        database:process.env.DB
+        database:process.env.DB,
+        uri:"mysql://u7exgb84kaqt9ci0:vEhnK7T9KQSfshRffF2E@bisy3vus6cp8hqicxxv5-mysql.services.clever-cloud.com:3306/bisy3vus6cp8hqicxxv5"
     }
 );
 
