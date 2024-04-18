4ade3858dfab369d8e41a828937a3bbcd56cd693
diego7547 <110865883+diego7547@users.noreply.github.com>
2024-04-17 19:44:00 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -7,9 +7,9 @@ const conexion = createPool(
     {
          host:"bisy3vus6cp8hqicxxv5-mysql.services.clever-cloud.com",
         port:"3306",
-        user:"u7exgb84kaqt9ci0",
-        password:"vEhnK7T9KQSfshRffF2E",
-        database:"bisy3vus6cp8hqicxxv5", 
+        user:"u7exgb84k",
+        password:"vEhnK7T9KQSfshR",
+        database:"bisy3vus6cp8hqic", 
        /*  host:"localhost",
         port:"3306",
         user:"root",
